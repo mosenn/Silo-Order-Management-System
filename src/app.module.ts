53dee46e8f93df1206cdb630b7c4fd29b324ec30
unknown <mohseen_abasi@yahoo.com>
2024-11-28 20:52:17 +0330

    add Jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,13 +8,17 @@ import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
+import { OrderService } from './order/order.service';
+import { OrderController } from './order/order.controller';
+import { OrderModule } from './order/order.module';
 @Module({
   imports: [
     PrismaModule,
     AuthModule,
     JwtModule.register({ secret: process.env.JWT_SECRET, signOptions: { expiresIn: '1d' } }),
+    OrderModule,
   ],
-  controllers: [AppController, AuthController],
-  providers: [AppService, PrismaService, FarmerService, AuthService],
+  controllers: [AppController, AuthController, OrderController],
+  providers: [AppService, PrismaService, FarmerService, AuthService, OrderService],
 })
 export class AppModule {}
