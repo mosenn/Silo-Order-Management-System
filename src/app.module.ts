59256eca1b31d48db546e86d282bc10ad10675ab
unknown <mohseen_abasi@yahoo.com>
2024-11-28 18:04:30 +0330

    define role / config app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
-
+import { FarmerService } from './farmer/farmer.service';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [PrismaModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    JwtModule.register({ secret: process.env.JWT_SECRET, signOptions: { expiresIn: '1d' } }),
+  ],
+  controllers: [AppController, AuthController],
+  providers: [AppService, PrismaService, FarmerService, AuthService],
 })
 export class AppModule {}
