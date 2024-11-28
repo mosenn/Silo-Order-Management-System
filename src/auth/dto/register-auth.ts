836a571c574ed7b6e46b0755cd7e3ca8c4d4398f
unknown <mohseen_abasi@yahoo.com>
2024-11-28 20:55:03 +0330

    add register-dto

--- src/auth/dto/register-auth.ts
+++ src/auth/dto/register-auth.ts
@@ -1,6 +1,10 @@
+// import { Role } from '@prisma/client';
+import { Role } from '@prisma/client';
 import {
   IsEmail,
+  IsEnum,
   IsNotEmpty,
+  IsOptional,
   IsString,
   Matches,
   MinLength,
@@ -10,6 +14,10 @@ export class registerDto {
   @IsString()
   @IsNotEmpty()
   username: string;
+  @IsString()
+  @IsOptional()
+  @IsEnum(Role, { message: 'Role must be either Farmer or Admin' })
+  role?: Role;
 
   @IsString()
   @IsNotEmpty()
@@ -21,7 +29,10 @@ export class registerDto {
   @MinLength(8)
   @Matches(
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-    { message: "(@$!%*#?&) رمز عبور باید حداقل ۸ کاراکتر باشد و شامل یک حرف کوچک، یک حرف بزرگ، یک عدد و یک کاراکتر خاص " },
+    {
+      message:
+        '(@$!%*#?&) رمز عبور باید حداقل ۸ کاراکتر باشد و شامل یک حرف کوچک، یک حرف بزرگ، یک عدد و یک کاراکتر خاص ',
+    },
   )
   password: string;
 }
