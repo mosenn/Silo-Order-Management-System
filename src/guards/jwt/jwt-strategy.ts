f523625f6ea5e9f676fbaf9aa2ff7a61776a7c0a
unknown <mohseen_abasi@yahoo.com>
2024-11-28 18:01:41 +0330

    add guard

---
+++ src/guards/jwt/jwt-strategy.ts
@@ -0,0 +1,24 @@
+import { Strategy, ExtractJwt } from 'passport-jwt';
+
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+  async validate(payload: {
+    id: string;
+    email: string;
+    username: string;
+    // role: string;
+  }) {
+    // console.log(payload, 'payload');
+    return { ...payload };
+  }
+}
