4924090c3a7daaa438ebf7d95aed4d94e8bae99b
unknown <mohseen_abasi@yahoo.com>
2024-12-04 16:21:32 +0330

    confg socket redis

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -2,18 +2,25 @@ import { Module } from '@nestjs/common';
 import { createClient } from 'redis';
 
 @Module({
-    providers:[
-        {
-            provide:"REDIS_CLIENT",
-            useFactory:async()=>{
-                const clinet = createClient({
-                    url:process.env.REDIS_URL,
-                });
-                await clinet.connect();
-                return clinet;
-            }
-        }
-    ],
-    exports: ['REDIS_CLIENT'],
+  providers: [
+    {
+      provide: 'REDIS_CLIENT',
+      useFactory: async () => {
+        const clinet = createClient({
+          url: process.env.REDIS_URL,
+          socket: {
+            tls: true,
+            rejectUnauthorized: false,
+          },
+        });
+        clinet.on('error', (err) => {
+            console.log('Redis Client Error', err);
+        });
+        await clinet.connect();
+        return clinet;
+      },
+    },
+  ],
+  exports: ['REDIS_CLIENT'],
 })
 export class RedisModule {}
